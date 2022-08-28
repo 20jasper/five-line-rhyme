aefc5995ce89b546f7c302f51fe6c92ed55a9e30
Jacob Asper <jacobasper191@gmail.com>
2022-08-28 16:57:20 -0400

    add getPoems and postPoems function

---
+++ controllers/poem.js
@@ -0,0 +1,22 @@
+const Poem = require('../models/Poem');
+
+/**
+ * GET /poems
+ * 
+ */
+
+exports.getPoems = (req, res) => {
+	res.render('error/500')
+}
+
+//add story post request
+exports.postPoem = async (req, res) => {
+	try {
+		req.body.user = req.user.userName
+		await Poem.create(req.body)
+		res.redirect('/')
+	} catch (err) {
+		console.error(err)
+		res.render('error/500')
+	}
+}

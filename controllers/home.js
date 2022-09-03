6150cff05411901a7c76c9ba7bb1e9694eb3b422
Jacob Asper <jacobasper191@gmail.com>
2022-09-02 22:48:09 -0400

    add function to render poems in the home page

--- controllers/home.js
+++ controllers/home.js
@@ -1,9 +1,24 @@
+const Poem = require('../models/Poem');
+
 /**
  * GET /
  * Home page.
  */
-exports.index = (req, res) => {
-  res.render('home', {
-    title: 'Home'
-  });
-};
+exports.index = async (req, res) => {
+	try {
+		//get all public stories
+		const poems = await Poem.find()
+			.sort({ createdAt: 'desc' })
+			.lean()
+		console.log(poems)
+		res.render('home', {
+			title: 'Home',
+			poems,
+		})
+	}
+	catch (err) {
+		console.error(err)
+		res.render('error/500')
+	}
+
+}

adfd6669944a2cf5195b55ce8bd0a3f1208c54a8
Jacob Asper <jacobasper191@gmail.com>
2022-10-25 11:52:17 -0400

    format file

--- controllers/poem.js
+++ controllers/poem.js
@@ -2,31 +2,31 @@ const Poem = require('../models/Poem');
 
 /**
  * GET /poems/add
- * 
+ *
  */
 
 exports.getAddPage = (req, res) => {
-	res.render("poems/add")
-}
+	res.render('poems/add');
+};
 
 /**
  * Post /poems
- * 
+ *
  */
 
-//add story post request
+// add story post request
 exports.postPoem = async (req, res, next) => {
 	try {
-		req.body.user = req.user.id
+		req.body.user = req.user.id;
 
 		const validationErrors = [];
-		const poem = req.body.content.trim()
+		const poem = req.body.content.trim();
 
 		if (poem.length > 500) validationErrors.push({ msg: 'Your Poem Is Longer than 500 characters' });
 
-		//if there are 0 new line characters, the length will be undefined
-		const newLineCount = poem.match(/\r/g)?.length ?? 0
-		//check against 4 since the first line doesn't have a new line character
+		// if there are 0 new line characters, the length will be undefined
+		const newLineCount = poem.match(/\r/g)?.length ?? 0;
+		// check against 4 since the first line doesn't have a new line character
 		if (newLineCount < 4) validationErrors.push({ msg: 'Your Poem Has Less Than 5 Lines' });
 		else if (newLineCount > 4) validationErrors.push({ msg: 'Your Poem Has More Than 5 Lines' });
 		if (validationErrors.length) {
@@ -34,22 +34,21 @@ exports.postPoem = async (req, res, next) => {
 			return res.redirect('/poems/add');
 		}
 
-		//sanatize poems
+		// sanatize poems
 		const entityHashMap = {
-			"&": "&amp;",
-			"<": "&lt;",
-			">": "&gt;",
-		}
-		req.body.content = req.body.content.replace(/&|<|>/g, (str) => entityHashMap[str])
+			'&': '&amp;',
+			'<': '&lt;',
+			'>': '&gt;',
+		};
+		req.body.content = req.body.content.replace(/&|<|>/g, (str) => entityHashMap[str]);
 
-		await Poem.create(req.body)
-		res.redirect('/')
+		await Poem.create(req.body);
+		res.redirect('/');
 	} catch (err) {
-		console.error(err)
-		res.render('error/500')
+		console.error(err);
+		res.render('error/500');
 	}
-}
-
+};
 
 /**
  * POST /id/delete
@@ -58,12 +57,12 @@ exports.postPoem = async (req, res, next) => {
 exports.postDeletePoem = (req, res, next) => {
 	// //delete profile picture on account deletion
 	try {
-		const validationErrors = []
+		const validationErrors = [];
 
-		//check if user is the same as who wrote the poem
+		// check if user is the same as who wrote the poem
 		Poem.findById(req.params.id, (err, poem) => {
 			if (err) { return next(err); }
-			console.log(poem.user.id, req.user.id)
+			console.log(poem.user.id, req.user.id);
 			if (poem.user.id !== req.user.id) validationErrors.push({ msg: 'User IDs do not match' });
 		});
 
@@ -77,17 +76,16 @@ exports.postDeletePoem = (req, res, next) => {
 			req.flash('info', { msg: 'Your poem has been deleted.' });
 			res.redirect('/');
 		});
-	}
-	catch (err) {
+	} catch (err) {
 		console.log(err);
 		req.flash('errors', { msg: 'Poem Deletion Failed' });
-		return res.redirect(`poems/${req.params.id}`)
+		return res.redirect(`poems/${req.params.id}`);
 	}
 };
 
 /**
  * GET /poems/add
- * 
+ *
  */
 
 exports.getPoem = async (req, res) => {
@@ -95,10 +93,10 @@ exports.getPoem = async (req, res) => {
 		const poem = await Poem.findById(req.params.id)
 			.populate('user');
 
-		res.render("poems/poem", {
+		res.render('poems/poem', {
 			poem,
 		});
 	} catch (err) {
 		console.log(err);
 	}
-}
+};

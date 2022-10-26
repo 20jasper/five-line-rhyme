96c7b5e78056e6fb7fe4174951df08b88d412a19
Jacob Asper <jacobasper191@gmail.com>
2022-10-26 02:16:19 -0400

    add function to get csrf token

--- helpers/testHelpers.test.js
+++ helpers/testHelpers.test.js
@@ -1,7 +1,14 @@
 const { MongoMemoryServer } = require('mongodb-memory-server');
+const cheerio = require('cheerio');
 
 exports.mongoMemoryServerConnect = async () => {
 	const mongoServer = await MongoMemoryServer.create();
 	const mockMongoDBUri = mongoServer.getUri();
 	process.env.MONGODB_URI = mockMongoDBUri;
+};
+
+exports.getCSRFToken = (res) => {
+	const $ = cheerio.load(res.text);
+	return $('meta[name="csrf-token"]')
+		.attr('content');
 };

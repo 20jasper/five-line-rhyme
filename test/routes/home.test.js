9be9d84780da466b4ab6b537793ce20d6d8f8861
Jacob Asper <jacobasper191@gmail.com>
2022-10-26 00:07:59 -0400

    change mongoMemoryConnect url

--- test/routes/home.test.js
+++ test/routes/home.test.js
@@ -1,6 +1,6 @@
 const request = require('supertest');
 const app = require('../../app');
-const { mongoMemoryServerConnect } = require('../../helpers/testHelpers..test');
+const { mongoMemoryServerConnect } = require('../../helpers/testHelpers.test');
 
 (() => {
 	before(mongoMemoryServerConnect);

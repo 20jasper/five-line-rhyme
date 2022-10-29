0719a010075615e7d53729c4a4d5d45144f0f66e
Jacob Asper <jacobasper191@gmail.com>
2022-10-29 02:34:31 -0400

    add poem controller test

---
+++ test/controllers/poem.test.js
@@ -0,0 +1,12 @@
+const { expect } = require('chai');
+const { sanitizeHTML } = require('../../helpers/poemHelpers');
+
+describe('Post poem', () => {
+	it('Should replace "&," "<," and ">" with HTML entities', (done) => {
+		let poem = 'here is a dangerous poem with spooky characters like &, <, and >';
+		poem = sanitizeHTML(poem);
+
+		expect(poem).to.equal('here is a dangerous poem with spooky characters like &amp;, &lt;, and &gt');
+		done();
+	});
+});

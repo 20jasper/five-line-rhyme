f7af206423dab7956a53c78eafed20bfed0c6bd4
Jacob Asper <jacobasper191@gmail.com>
2022-11-20 16:22:09 -0500

    replace node sass with sass

--- app.js
+++ app.js
@@ -14,7 +14,6 @@ const flash = require('express-flash');
 const path = require('path');
 const mongoose = require('mongoose');
 const passport = require('passport');
-const sass = require('node-sass-middleware');
 const upload = require('./middleware/multer');
 const passportConfig = require('./config/passport');
 
@@ -50,10 +49,6 @@ app.set('port', process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 app.use(compression());
-app.use(sass({
-	src: path.join(__dirname, 'public'),
-	dest: path.join(__dirname, 'public')
-}));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
